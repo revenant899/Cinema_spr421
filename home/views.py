c8b2fa7b78ca82b758630fae055c18b14bf31b5a
Мар'ян <'@DESKTOP-V3EADRN>
2026-02-04 21:18:12 +0200

    filter + admin panel

--- home/views.py
+++ home/views.py
@@ -4,11 +4,11 @@ from favorites.favorites import get_favorite_films
 from films.models import Film
 
 
-
 def index(request, filter_by_favorites=False):
     films = Film.objects.all()
 
     filter_text = request.GET.get("filter_text", "")
+    category_filter = request.GET.get("category", "")
 
     if filter_by_favorites:
         fav_ids = get_favorite_films(request)
@@ -16,8 +16,13 @@ def index(request, filter_by_favorites=False):
 
     if filter_text:
         films = films.filter(name__icontains=filter_text)
+    
+    if category_filter:
+        films = films.filter(category=category_filter)
 
     return render(request, 'home/index.html', {
         'films': films, 
         "fav_ids": get_favorite_films(request),
+        "categories": Film.CATEGORY_CHOICES,
+        "selected_category": category_filter,
     })
