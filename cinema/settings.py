735fda4d8544dd4bdfb607435344a0627fc8f9b1
Мар'ян <'@DESKTOP-V3EADRN>
2026-02-07 08:05:34 +0200

    rating + type(2D, 3D) + netflix style

--- cinema/settings.py
+++ cinema/settings.py
@@ -132,36 +132,36 @@ MEDIA_LOCATION = "media"
 
 
 
-# ---- Azure Blob Storage
-AZURE_CONTAINER_STATIC = "static"
-AZURE_CONTAINER_MEDIA = "media"
+# # ---- Azure Blob Storage
+# AZURE_CONTAINER_STATIC = "static"
+# AZURE_CONTAINER_MEDIA = "media"
 
-AZURE_ACCOUNT_NAME = "storageforcinema"
-AZURE_CUSTOM_DOMAIN = f"{AZURE_ACCOUNT_NAME}.blob.core.windows.net"
-STATIC_URL = f"https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER_STATIC}/"
-MEDIA_URL = f"https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER_MEDIA}/"
+# AZURE_ACCOUNT_NAME = "storageforcinema"
+# AZURE_CUSTOM_DOMAIN = f"{AZURE_ACCOUNT_NAME}.blob.core.windows.net"
+# STATIC_URL = f"https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER_STATIC}/"
+# MEDIA_URL = f"https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER_MEDIA}/"
 
-# ----
-AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY", "")
-AZURE_CONNECTION_STRING = os.getenv("AZURE_CONNECTION_STRING", "")
+# # ----
+# AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY", "")
+# AZURE_CONNECTION_STRING = os.getenv("AZURE_CONNECTION_STRING", "")
 
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.azure_storage.AzureStorage",
-        "OPTIONS": {
-            "azure_container": AZURE_CONTAINER_MEDIA,
-            "account_name": AZURE_ACCOUNT_NAME,
-            "account_key": AZURE_ACCOUNT_KEY,
-            "connection_string": AZURE_CONNECTION_STRING,
-        },
-    },
-    "staticfiles": {
-        "BACKEND": "storages.backends.azure_storage.AzureStorage",
-        "OPTIONS": {
-            "azure_container": AZURE_CONTAINER_STATIC,
-            "account_name": AZURE_ACCOUNT_NAME,
-            "account_key": AZURE_ACCOUNT_KEY,
-            "connection_string": AZURE_CONNECTION_STRING,
-        },
-    },
-}
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "storages.backends.azure_storage.AzureStorage",
+#         "OPTIONS": {
+#             "azure_container": AZURE_CONTAINER_MEDIA,
+#             "account_name": AZURE_ACCOUNT_NAME,
+#             "account_key": AZURE_ACCOUNT_KEY,
+#             "connection_string": AZURE_CONNECTION_STRING,
+#         },
+#     },
+#     "staticfiles": {
+#         "BACKEND": "storages.backends.azure_storage.AzureStorage",
+#         "OPTIONS": {
+#             "azure_container": AZURE_CONTAINER_STATIC,
+#             "account_name": AZURE_ACCOUNT_NAME,
+#             "account_key": AZURE_ACCOUNT_KEY,
+#             "connection_string": AZURE_CONNECTION_STRING,
+#         },
+#     },
+# }
--- films/models.py
+++ films/models.py
@@ -1,6 +1,11 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Film(models.Model):
+    TYPE_CHOICES = [
+        ('2D', '2D'),
+        ('3D', '3D'),
+    ]
     CATEGORY_CHOICES = [
         ('action', 'Action'),
         ('drama', 'Drama'),
@@ -8,6 +13,7 @@ class Film(models.Model):
         ('horror', 'Horror'),
         ('sci-fi', 'Sci-Fi'),
     ]
+
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='film_images/', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
@@ -15,6 +21,8 @@ class Film(models.Model):
     stock = models.IntegerField()
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     show_time = models.DateTimeField()
+    star = models.IntegerField(default=0,validators=[MinValueValidator(0), MaxValueValidator(5)], blank=True)
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES, default='2D')
 
     def __str__(self):
         return self.name
